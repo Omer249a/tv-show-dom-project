2bb598ee192079f48c65e59a3877cb0cd01c12a5
Omer249a <omer249aa@gmail.com>
2021-09-04 15:04:14 +0100

    new updates

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
+ 
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
   episodeList.forEach(createCard);
@@ -17,6 +19,22 @@ function createCard(episode) {
   console.log(episode.season);
   console.log(episode.number);
    console.log(episode.summary);
+
+   let card = document.createElement("span");
+   let cardTitle = document.createElement("h3");
+   cardTitle.innerText = episode.name;
+   card.appendChild(cardTitle);
+
+   let cardImage = document.createElement("img");
+   cardImage.src = episode.image.medium;
+   card.appendChild(cardImage);
+   
+   let cardSummary = document.createElement("div");
+   cardSummary.innerHTML = episode.summary;
+   card.appendChild(cardSummary);
+
+   rootElem.appendChild(card);
+
 }
 
 
